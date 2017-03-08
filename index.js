5b6c70ae9a26d1734c06758063c42f4f0d5fc8c5
Leko <leko.noor@gmail.com>
2017-03-08 20:30:08 +0900

    chore: Add empty files & index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import InProgress from './src/InProgress'
+import NullDetector from './src/NullDetector'
+import ObjectDetector from './src/ObjectDetector'
+import FormValueDetector from './src/FormValueDetector'
+import PromiseDetector from './src/PromiseDetector'
+
+export default InProgress
+export {
+  NullDetector,
+  ObjectDetector,
+  FormValueDetector,
+  PromiseDetector
+}
