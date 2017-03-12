027212e4366d8fc18de2a75c5bcc7f1b0c043fc0
Leko <leko.noor@gmail.com>
2017-03-12 23:25:15 +0900

    feat: Remove unused file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import InProgress from './src/InProgress'
 import NullDetector from './src/NullDetector'
 import FormValueDetector from './src/FormValueDetector'
 
-export default InProgress
 export {
   NullDetector,
   FormValueDetector
