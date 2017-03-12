973ec56156781d42eb29e4b7613ddfaa9b8db7e9
Leko <leko.noor@gmail.com>
2017-03-13 00:16:22 +0900

    feat: Split observe to handleBeforeUnload

--- src/NullDetector.js
+++ src/NullDetector.js
@@ -5,26 +5,28 @@ export default class NullDetector {
   constructor (valueFn) {
     this.valueFn = valueFn
     this.initialValues = {}
+    this.handleBeforeUnload = this.handleBeforeUnload.bind(this)
+  }
+
+  handleBeforeUnload (e) {
+    if (this.inProgress()) {
+      e.returnValue = this.customMessage
+      e.preventDefault()
+      return this.customMessage
+    } else {
+      return null
+    }
   }
 
   observe (customMessage) {
     this.reset()
-    this.listener = window.addEventListener('beforeunload', e => {
-      if (this.inProgress()) {
-        e.returnValue = customMessage
-        e.preventDefault()
-        return customMessage
-      } else {
-        return null
-      }
-    }, false)
+    this.customMessage = customMessage
+    window.addEventListener('beforeunload', this.handleBeforeUnload, false)
   }
 
   stopObserve () {
-    if (!this.listener) {
-      throw new Error('Not listening')
-    }
-    this.listener.destroy()
+    window.removeEventListener('beforeunload', this.handleBeforeUnload, false)
+    this.customMessage = null
   }
 
   reset () {
