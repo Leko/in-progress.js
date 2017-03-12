016f9df74089af2d4da4dbca43611879cb180dfe
Leko <leko.noor@gmail.com>
2017-03-12 23:30:43 +0900

    test: Add describes

---
+++ test/FormValueDetector.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+const { FormValueDetector } = require('../dist/inprogress')
+
+describe(FormValueDetector.name, () => {
+  describe('#constructor', () => {
+
+  })
+  describe('#getValues', () => {
+
+  })
+})
---
+++ test/NullDetector.spec.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+const { NullDetector } = require('../dist/inprogress')
+
+describe(NullDetector.name, () => {
+  describe('#constructor', () => {
+
+  })
+  describe('#observe', () => {
+
+  })
+  describe('#stopObserve', () => {
+
+  })
+  describe('#reset', () => {
+
+  })
+  describe('#inProgress', () => {
+
+  })
+  describe('#hasChanges', () => {
+
+  })
+})
